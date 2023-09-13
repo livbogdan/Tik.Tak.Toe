d2cc66b5daf8692a7e4511a0fa50a871a0b9eaaa
livbogdan <livbogdan@gmail.com>
2023-09-13 11:22:52 +0200

    Final Stage of PVP mode
    Added:
    Visualization of player movement.
    
    A scoring system to keep track of how many times each player has won

--- Tik.Tak.Toe/Controller/PVPGameViewController.swift
+++ Tik.Tak.Toe/Controller/PVPGameViewController.swift
@@ -9,7 +9,6 @@ class PVPGameViewController: UIViewController {
     
     @IBOutlet weak var lblScore: UILabel!
     
-    
     @IBOutlet weak var lblPlayer1: UILabel!
     @IBOutlet weak var lblPlayer2: UILabel!
     
@@ -17,6 +16,9 @@ class PVPGameViewController: UIViewController {
     
     var p1receivedText: String?
     var p2receivedText: String?
+    
+    var p1Score = 0
+    var p2Score = 0
     
     var currentPlayer: Player = .X
     
@@ -73,7 +75,8 @@ class PVPGameViewController: UIViewController {
                 gameBoard[i] = nil
                 if let imageView = view.viewWithTag(i) as? UIImageView {
                     imageView.image = backgroundImage
-                    print(" Image with tag \(imageView.tag) is reset.")
+                    imageView.isUserInteractionEnabled = true
+                    print("Image with tag \(imageView.tag) is reset.")
                 }
             }
             
@@ -86,17 +89,31 @@ class PVPGameViewController: UIViewController {
 //                    print("swap background")
 //                }
 //            }
-            print("game restarted")
+            print("Game is Restarted")
             currentPlayer = .X
         }
     
     func showResult(_ result: String) {
         let alertController = UIAlertController(title: "Game Over", message: result, preferredStyle: .alert)
-        let restartAction = UIAlertAction(title: "Restart", style: .default) { _ in self.resetGameBoard()
-        }
+            
+            let restartAction = UIAlertAction(title: "Restart", style: .default) { _ in
+                self.resetGameBoard()
+            }
+            
             alertController.addAction(restartAction)
+            
+            // Update scores based on the result
+            if result.contains("Player X is won!") {
+                p1Score += 1
+            } else if result.contains("Player O is won!") {
+                p2Score += 1
+            }
+            
+            lblScore.text = "Score: \(p1Score) - \(p2Score)" // Update the score label
+            
             present(alertController, animated: true, completion: nil)
         }
+    
     
     @IBAction func tapped(_ sender: UITapGestureRecognizer) {
         if gameOver {
@@ -114,10 +131,21 @@ class PVPGameViewController: UIViewController {
                     imageView.image = UIImage(named: "Player1")
                     gameBoard[imageViewTag] = .X
                     print("X Cell with tag \(imageViewTag) was tapped.")
+                    
+                    // Change the background color of Player 1's label to green
+                    lblPlayer2.backgroundColor = UIColor.green
+                    lblPlayer1.backgroundColor = UIColor.clear
+                    print("Player 1 green")
+                    
                 } else {
                     imageView.image = UIImage(named: "Player2")
                     gameBoard[imageViewTag] = .O
                     print("0 Cell with tag \(imageViewTag) was tapped.")
+                    
+                    // Change the background color of Player 2's label to green
+                    lblPlayer1.backgroundColor = UIColor.green
+                    lblPlayer2.backgroundColor = UIColor.clear
+                    print("Player 2 green")
                 }
                 
                 if checkForWin() {
